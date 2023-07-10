1b2cb9bad5dcf3ff636dc18ff4c2cdd844e1815d
Aman Sagar <109031753+Amansagarmithu@users.noreply.github.com>
2023-07-10 23:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+//your code her
+let input = document.getElementById("evaluatedText");
+let heading = document.getElementById("letterCount");
+
+function change(){
+	let num = input.value.length;
+	heading.innerText = num;
+}
+
+input.onchange = change;
+
